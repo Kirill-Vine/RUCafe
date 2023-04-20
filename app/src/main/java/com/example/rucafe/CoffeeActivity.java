b3866e47290f16c2fa584a878fdc8a552a12f508
Kirill Vinogradov <61331094+Kirill-Vine@users.noreply.github.com>
2023-04-20 16:41:40 -0400

    functionality for coffee and main activities

--- app/src/main/java/com/example/rucafe/CoffeeActivity.java
+++ app/src/main/java/com/example/rucafe/CoffeeActivity.java
@@ -3,12 +3,66 @@ package com.example.rucafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CoffeeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CoffeeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private double price = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee);
+        Spinner sizeSpinner = (Spinner) findViewById(R.id.sizeSpinner);
+
+        sizeSpinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("SHORT");
+        categories.add("TALL");
+        categories.add("GRANDE");
+        categories.add("VENTI");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        sizeSpinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
+        if(item.equals("SHORT")){
+            price=1.89;
+        }else if(item.equals("TALL")){
+            price=2.29;
+        }else if(item.equals("GRANDE")){
+            price=2.69;
+        }else if(item.equals("VENTI")){
+            price=3.09;
+        }
+
+        changeSubtotal("Subtotal: ", price);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void changeSubtotal(String subTotalText, double price){
+
+        TextView subtotal = (TextView)findViewById(R.id.subTotalText);
+        subtotal.setText(subTotalText+" "+price);
     }
 }
--- app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -4,12 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton cofeeImageButton;
+    ImageButton donutImageButton;
+    ImageButton cartImageButton;
+    ImageButton ordersImageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cofeeImageButton = (ImageButton) findViewById(R.id.coffeeImageButton);
+        cofeeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCoffeeActivity();
+            }
+        });
+        donutImageButton = (ImageButton) findViewById(R.id.donutImageButton);
+        donutImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDonutActivity();
+            }
+        });
+        cartImageButton = (ImageButton) findViewById(R.id.cartImageButton);
+        cartImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCartActivity();
+            }
+        });
+        ordersImageButton = (ImageButton) findViewById(R.id.ordersImageButton);
+        ordersImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOrdersActivity();
+            }
+        });
+
     }
+    public void openCoffeeActivity(){
+        Intent intent = new Intent(this, CoffeeActivity.class);
+        startActivity(intent);
+    }
+    public void openDonutActivity(){
+        Intent intent = new Intent(this, DonutActivity.class);
+        startActivity(intent);
+    }
+    public void openCartActivity(){
+        Intent intent = new Intent(this, CartActivity.class);
+        startActivity(intent);
+    }
+    public void openOrdersActivity(){
+        Intent intent = new Intent(this, OrdersActivity.class);
+        startActivity(intent);
+    }
+
 }
