13f2c54f9d5635c4aa216189b20ed9f7b1b48541
Kirill Vinogradov <61331094+Kirill-Vine@users.noreply.github.com>
2023-04-21 17:13:00 -0400

    Update CartActivity.java

--- app/src/main/java/com/example/rucafe/CartActivity.java
+++ app/src/main/java/com/example/rucafe/CartActivity.java
@@ -2,7 +2,6 @@ package com.example.rucafe;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,47 +15,54 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class CartActivity extends Activity {
-//    ArrayList<String> orderStringList = new ArrayList<>();
     ArrayList<MenuItem> orderItems = MainActivity.currentOrder.getItems();
-    String orderStringList [] = new String[orderItems.size()];
     private Button removeSelected;
-//    private MenuItem selectedItem;
+    private String selectedItem;
+    private ListView listView;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
         removeSelected = (Button) findViewById(R.id.removeItem);
+        showList();
+        displayTotal();
+
+
+
+        removeSelected.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                for (int i = 0; i < orderItems.size(); i++) {
+                    if(orderItems.get(i).toString().equals(selectedItem)){
+                        MainActivity.currentOrder.removeItem(orderItems.get(i));
+                    }
+                }
+                displayTotal();
+                showList();
+            }
+        });
+    }
+    private void showList(){
+        String orderStringList [] = new String[orderItems.size()];
         for(int i = 0; i < orderItems.size(); i++) {
-//            orderStringList.add(orderItems.get(i).toString());
             orderStringList[i]=orderItems.get(i).toString();
         }
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+        adapter = new ArrayAdapter<String>(this,
                 R.layout.activity_listview, orderStringList);
 
-        ListView listView = (ListView) findViewById(R.id.ordersList);
+        listView = (ListView) findViewById(R.id.ordersList);
         listView.setAdapter(adapter);
-        displayTotal();
         listView.setOnItemClickListener(new OnItemClickListener()
         {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(CartActivity.this, orderStringList[position], Toast.LENGTH_SHORT).show();
-                String selectedItem = (String) adapter.getItem(position);
-//                Log.d("", item);
+                selectedItem = (String) adapter.getItem(position);
             }
         });
 
-
-//        removeSelected.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                MainActivity.currentOrder.removeItem(selectedItem);
-////                Toast.makeText(v.getContext(),currentCoffee.toString() + " added to order", Toast.LENGTH_LONG).show();
-//                Log.d("","removed!");
-//                displayTotal();
-//            }
-//        });
     }
     private void displayTotal() {
         DecimalFormat df = new DecimalFormat("#,###.00");
